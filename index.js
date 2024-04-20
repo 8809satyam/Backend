ae09d2da994e3f4149f8a20b2a34ac2bfac2dc43
Satyam Singh <129529397+8809satyam@users.noreply.github.com>
2024-04-20 17:45:30 +0530

    add files to backend

---
+++ index.js
@@ -0,0 +1,18 @@
+//console.log("Har Har Mahadev");
+require('dotenv').config()
+const express=require('express')
+const app=express()
+const port=3000
+
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+})
+app.get('/twitter',(req,res)=>{
+  res.send('<h1>Elon Musk</h1>')
+})
+app.get('/youtube',(req,res)=>{
+  res.send("<h2>Ram Ram Bhai Sarya Ne </h2>")
+})
+app.listen(process.env.PORT,()=>{
+    console.log(`Example app listening on port ${port}`)
+})
